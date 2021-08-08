ece7db6d8a02b80655a508ff7f698dede2c2382c
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-08 20:30:40 +0300

    :sparkles: feat: parse json in post and put requests

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
