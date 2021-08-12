d644bf6a5f2893a06846a3a4b06149faf3e5c990
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-13 00:05:02 +0300

    :sparkles: feat: create user details route

--- backend/src/routers/user/index.ts
+++ backend/src/routers/user/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import registerRoute from './registerRoute';
 import loginRoute from './loginRoute';
 import confirmUserRoute from './confirmUserRoute';
+import userDetailsRoute from './userDetailsRoute';
 
 const userRouter = express.Router();
 
 userRouter.use('/users', registerRoute);
 userRouter.use('/users', loginRoute);
+userRouter.use('/users', userDetailsRoute);
 userRouter.use('/users', confirmUserRoute);
 
 export default userRouter;
---
+++ backend/src/routers/user/userDetailsRoute.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import User from '../../models/userModel';
+import logger from '../../utils/logger';
+
+const router = express.Router();
+
+router.get(
+  '/:id',
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const userId = req.params.id;
+
+    if (!userId) {
+      res.status(404).send({ message: 'Request missing user Id' });
+      logger.error(
+        `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Request failed due to missing user Id`
+      );
+      return;
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send({ message: 'User Not Found' });
+      logger.error(
+        `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Request failed due to user not found`
+      );
+      return;
+    }
+
+    res.status(200).send({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phone: user.phone
+    });
+  })
+);
+
+export default router;
