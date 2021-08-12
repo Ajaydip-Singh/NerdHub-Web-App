08d956f1566e33f6729504f76977bdd290357d86
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-12 18:07:42 +0300

    :sparkles: feat: use mailgen for creating email template

--- backend/src/utils/mail/mail.ts
+++ backend/src/utils/mail/mail.ts
@@ -1,7 +1,16 @@
 import mailgun, { Mailgun } from 'mailgun-js';
+import Mailgen from 'mailgen';
 
 export const mg = (): Mailgun =>
   mailgun({
     apiKey: String(process.env.MG_API_KEY),
     domain: String(process.env.MG_DOMAIN)
   });
+
+export const mailGenerator = new Mailgen({
+  theme: 'default',
+  product: {
+    name: 'NerdHub Kenya',
+    link: 'http://localhost:3000/home'
+  }
+});
