c3ed27929de2041497ce62f580b6c9007198ef53
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-17 11:34:19 +0300

    :card_file_box: chore: update events model

--- backend/src/models/eventModel.ts
+++ backend/src/models/eventModel.ts
@@ -7,16 +7,16 @@ const eventSchema = new mongoose.Schema(
     date: { type: Date, required: true },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
-    price: { type: Boolean, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
     isFeaturedEvent: { type: Boolean, default: false, required: true },
     country: { type: String, default: 'Kenya', required: true },
     city: { type: String, default: 'Nairobi', required: true },
     venue: { type: String, required: true },
     category: { type: String, required: true },
-    isActive: { type: String },
+    isActive: { type: Boolean, default: false, required: true },
+    capacity: { type: Number, default: 10, required: true },
     registeredGuests: { type: [mongoose.Schema.Types.ObjectId], ref: 'User' },
-    actualNumberOfGuests: { type: Number },
-    capacity: { type: Number }
+    actualNumberOfGuests: { type: Number, default: 0 }
   },
   {
     timestamps: true
