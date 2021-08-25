cbf1c2e25a886d20ccddd56bf899446bf8f080db
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-25 18:07:48 +0300

    :card_file_box: chore: add home page content model

---
+++ backend/src/models/homePageModel.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const homePageSchema = new mongoose.Schema(
+  {
+    videoHeading: { type: String, required: true },
+    videUrl: { type: String, required: true },
+    videoThumbnail: { type: String, required: true },
+    videoBackgroundImage: { type: String, required: true },
+    videoBorderColor: { type: String, required: true },
+    videoBoxShadowColor: { type: String, required: true },
+    eventBackgroundImage: { type: String, required: true },
+    contactBackgroundColor: { type: String, required: true },
+    contactText: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const HomePageContent = mongoose.model('HomePageContent', homePageSchema);
+
+export default HomePageContent;
