80858da81baa3dd2b28ae08588b5c9ffe7618e53
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-07 22:23:53 +0300

    :sparkles: feat: convert image buffer to string using datauri

--- backend/src/interfaces/express.ts
+++ backend/src/interfaces/express.ts
@@ -4,3 +4,19 @@ import { JwtPayload } from 'jsonwebtoken';
 export interface GetUserAuthInfoRequest extends Request {
   user?: JwtPayload | undefined | any;
 }
+
+export interface MulterFile {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  buffer: {
+    type: string;
+    data: Array<number>;
+  };
+  size: number;
+}
+
+export interface FileRequest extends Request {
+  file?: MulterFile | undefined | any;
+}
--- backend/src/utils/upload.ts
+++ backend/src/utils/upload.ts
@@ -1,5 +1,21 @@
 import multer from 'multer';
+import DatauriParser from 'datauri/parser';
+import path from 'path';
+import { FileRequest } from '../interfaces/express';
 
 const storage = multer.memoryStorage();
 
 export const multerUploads = multer({ storage }).single('image');
+
+const parser = new DatauriParser();
+
+/**
+ * @description This function converts the buffer to data url
+ * @param {Object} req containing the field object
+ * @returns {String} The data url from the string buffer
+ */
+export const dataUri = (req: FileRequest): DatauriParser =>
+  parser.format(
+    path.extname(req.file.originalname).toString(),
+    req.file.buffer
+  );
