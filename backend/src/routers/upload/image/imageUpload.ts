5688292b1ee5050c3df86b96e3f22ece62207bcc
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-07 22:07:32 +0300

    :sparkles: feat: create multer middleware

--- backend/src/routers/upload/image/imageUpload.ts
+++ backend/src/routers/upload/image/imageUpload.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import { isAdmin, isAuth } from '../../../utils/general';
 import logger from '../../../utils/logger';
+import { multerUploads } from '../../../utils/upload';
 
 const router = express.Router();
 
@@ -9,11 +10,11 @@ router.post(
   '/',
   isAuth,
   isAdmin,
+  multerUploads,
   expressAsyncHandler(async (req: Request, res: Response) => {
-    const body = req;
-    console.log(body);
+    const file = req.file;
 
-    // res.send(body);
+    res.send(file);
 
     logger.info(
       `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Uploaded image succesfully.`
