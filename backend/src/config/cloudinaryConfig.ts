a43a87c803edb1d8ce0a0f903a03e2883e0a4212
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-07 23:16:52 +0300

    :sparkles: feat: change cloudinary config to middleware

--- backend/src/config/cloudinaryConfig.ts
+++ backend/src/config/cloudinaryConfig.ts
@@ -1,8 +1,17 @@
 import cloudinary from 'cloudinary';
+import { NextFunction, Request, Response } from 'express';
 
-export const cloudinaryConfig = (): any =>
+export const uploader = cloudinary.v2.uploader;
+
+export const cloudinaryConfig = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+): any => {
   cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
   });
+  next();
+};
