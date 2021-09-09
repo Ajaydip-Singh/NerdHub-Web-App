73c1826d11656b3c894c96936c072ae4abecdd95
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 23:44:04 +0300

    :sparkles: feat: create get products endpoint

--- backend/src/routers/product/index.ts
+++ backend/src/routers/product/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import getProductsRoute from './getProducts';
+import getProductsRoute from './getProductsRoute';
+import getProductRoute from './getProductRoute';
 
 const router = express.Router();
 
 router.use('/products', getProductsRoute);
+router.use('/products', getProductRoute);
 
 export default router;
