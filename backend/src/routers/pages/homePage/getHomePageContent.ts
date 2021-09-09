0591d7fc1a0abb44e17be9eea65af1976d116020
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 22:03:24 +0300

    :bug: fix: make make home page content get endpoint public

--- backend/src/routers/pages/homePage/getHomePageContent.ts
+++ backend/src/routers/pages/homePage/getHomePageContent.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import HomePageContent from '../../../models/homePageModel';
-import { isAdmin, isAuth } from '../../../utils/general';
 import logger from '../../../utils/logger';
 
 const router = express.Router();
 
 router.get(
   '/',
-  isAuth,
-  isAdmin,
   expressAsyncHandler(async (req: Request, res: Response) => {
     const homePageContent = await HomePageContent.findOne();
     res.send(homePageContent);
