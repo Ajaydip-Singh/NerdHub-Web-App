732e8b4c62299126955842c5330aa3d7c788fe24
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 23:49:52 +0300

    :sparkles: feat: create delete product endpoint

--- backend/src/routers/product/index.ts
+++ backend/src/routers/product/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import getProductsRoute from './getProductsRoute';
 import getProductRoute from './getProductRoute';
+import deleteProductRoute from './deleteProductRoute';
 
 const router = express.Router();
 
 router.use('/products', getProductsRoute);
 router.use('/products', getProductRoute);
+router.use('/products', deleteProductRoute);
 
 export default router;
