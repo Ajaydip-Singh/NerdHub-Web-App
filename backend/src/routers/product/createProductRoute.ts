41beb8f0d160351a3fcae6439733e88edd5c4c7a
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 23:54:15 +0300

    :sparkles: feat: add create product endpoint

---
+++ backend/src/routers/product/createProductRoute.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Product from '../../models/productModel';
+import { isAuth, isAdmin } from '../../utils/general';
+import logger from '../../utils/logger';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const product = new Product({
+      name: 'Sample product' + Date.now(),
+      image: '/images/call_of_duty_ghosts.jpeg',
+      brand: 'Sample brand',
+      category: 'Sample category',
+      description: 'Sample description',
+      price: 0,
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0
+    });
+
+    const createdproduct = await product.save();
+
+    res.send(createdproduct);
+
+    logger.info(
+      `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Created new product succesfully.`
+    );
+  })
+);
+
+export default router;
--- backend/src/routers/product/index.ts
+++ backend/src/routers/product/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import getProductsRoute from './getProductsRoute';
 import getProductRoute from './getProductRoute';
 import deleteProductRoute from './deleteProductRoute';
+import createProductRoute from './createProductRoute';
 
 const router = express.Router();
 
 router.use('/products', getProductsRoute);
 router.use('/products', getProductRoute);
 router.use('/products', deleteProductRoute);
+router.use('/products', createProductRoute);
 
 export default router;
