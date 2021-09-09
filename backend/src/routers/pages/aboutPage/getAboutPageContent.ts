34054c83db893b608f533a09757c28935e23cb8e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 22:04:36 +0300

    :bug: fix: make get page endpoints public

--- backend/src/routers/pages/aboutPage/getAboutPageContent.ts
+++ backend/src/routers/pages/aboutPage/getAboutPageContent.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import AboutPageContent from '../../../models/aboutPageModel';
-import { isAdmin, isAuth } from '../../../utils/general';
 import logger from '../../../utils/logger';
 
 const router = express.Router();
 
 router.get(
   '/',
-  isAuth,
-  isAdmin,
   expressAsyncHandler(async (req: Request, res: Response) => {
     const aboutPageContent = await AboutPageContent.findOne();
     res.send(aboutPageContent);
--- backend/src/routers/pages/contactPage/getContactPageContent.ts
+++ backend/src/routers/pages/contactPage/getContactPageContent.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import ContactPageContent from '../../../models/contactPageModel';
-import { isAdmin, isAuth } from '../../../utils/general';
 import logger from '../../../utils/logger';
 
 const router = express.Router();
 
 router.get(
   '/',
-  isAuth,
-  isAdmin,
   expressAsyncHandler(async (req: Request, res: Response) => {
     const contactPageContent = await ContactPageContent.findOne();
     res.send(contactPageContent);
