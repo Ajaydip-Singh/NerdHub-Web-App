5ec01b32385c3941552b0ad4bad6f931c42d5105
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-11 11:56:56 +0300

    :card_file_box: chore: update product model

--- backend/src/models/productModel.ts
+++ backend/src/models/productModel.ts
@@ -2,13 +2,15 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    cardName: { type: String, required: true, unique: true },
+    pageName: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    displayPrice: { type: String, required: true },
+    cardDisplayPrice: { type: String, required: true },
+    pageDisplayPrice: { type: String, required: true },
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
