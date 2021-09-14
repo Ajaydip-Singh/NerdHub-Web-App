b907c77c9293fd63c9f2715cfc2c2cfac0b3d37e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-14 12:16:42 +0300

    :sparkles: feat: add multiple image upload multer and datauri support

--- backend/src/utils/upload.ts
+++ backend/src/utils/upload.ts
@@ -5,7 +5,9 @@ import { FileRequest } from '../interfaces/express';
 
 const storage = multer.memoryStorage();
 
-export const multerUploads = multer({ storage }).single('image');
+export const multerSingleUpload = multer({ storage }).single('image');
+
+export const multerMultipleUpload = multer({ storage }).array('images', 5);
 
 const parser = new DatauriParser();
 
@@ -19,3 +21,16 @@ export const dataUri = (req: FileRequest): DatauriParser =>
     path.extname(req.file.originalname).toString(),
     req.file.buffer
   );
+
+export const multipleDataUri = (req: FileRequest): string[] => {
+  const data: string[] = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const data_url = parser.format(
+      path.extname(req.files[i].originalname).toString(),
+      req.files[i].buffer
+    );
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    data.push(data_url.content!);
+  }
+  return data;
+};
