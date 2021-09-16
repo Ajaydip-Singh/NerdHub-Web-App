cf5b2d19e6df91a66a838a43b2561f0346852204
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-16 15:53:51 +0300

    :recycle: refactor: change name of endpoints to order route

--- backend/src/routers/order/index.ts
+++ backend/src/routers/order/index.ts
@@ -4,17 +4,13 @@ import getProductRoute from './getOrderRoute';
 import deleteProductRoute from './deleteOrderRoute';
 import createProductRoute from './createOrderRoute';
 import editProductRoute from './editOrderDeliverRoute';
-import getProductsCategoriesRoute from './getProductsCategoriesRoute';
-import getProductsBrandRoute from './getProductsBrandsRoute';
 
 const router = express.Router();
 
-router.use('/products', getProductsCategoriesRoute);
-router.use('/products', getProductsBrandRoute);
-router.use('/products', getProductsRoute);
-router.use('/products', getProductRoute);
-router.use('/products', deleteProductRoute);
-router.use('/products', createProductRoute);
-router.use('/products', editProductRoute);
+router.use('/orders', getProductsRoute);
+router.use('/orders', getProductRoute);
+router.use('/orders', deleteProductRoute);
+router.use('/orders', createProductRoute);
+router.use('/orders', editProductRoute);
 
 export default router;
