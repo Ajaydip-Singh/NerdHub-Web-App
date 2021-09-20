5f9e2b984ebe66cba4ba90f1755af4ab587251e9
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-20 22:12:43 +0300

    :fire: chore: remove get images route

--- backend/src/routers/assets/image/index.ts
+++ backend/src/routers/assets/image/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import uploadImageRoute from './uploadImageRoute';
 import uploadMultipleImagesRoute from './uploadMultipleImagesRoute';
-import getImagesRoute from './getImagesRoute';
 
 const router = express.Router();
 
 router.use('/image', uploadMultipleImagesRoute);
 router.use('/image', uploadImageRoute);
-router.use('/image', getImagesRoute);
 
 export default router;
