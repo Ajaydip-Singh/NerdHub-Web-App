1d18d9273e0207e115441633d57a5b7b8172d98c
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-21 23:52:24 +0300

    :sparkles: feat: configure pagination in gallery screen

--- backend/src/routers/gallery/getGalleryRoute.ts
+++ backend/src/routers/gallery/getGalleryRoute.ts
@@ -8,15 +8,26 @@ const router = express.Router();
 router.get(
   '/',
   expressAsyncHandler(async (req: Request, res: Response) => {
+    const pageSize = 2;
+    const pageNumber = Number(req.query.pageNumber) || 1;
+    console.log(pageNumber);
+
     const tag = req.query.tag || '';
 
     const tagFilter = tag ? { tags: tag } : {};
-    const gallery = await Gallery.find({
+
+    const count = await Gallery.count({
       ...tagFilter
     });
 
+    const gallery = await Gallery.find({
+      ...tagFilter
+    })
+      .skip(pageSize * (pageNumber - 1))
+      .limit(pageSize);
+
     if (gallery.length !== 0) {
-      res.send(gallery);
+      res.send({ gallery, pageNumber, pages: Math.ceil(count / pageSize) });
       logger.info(
         `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Gallery sent succesfully`
       );
