7011f4d5ea25e057097918fb02fa38670a490ce5
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-22 11:58:39 +0300

    :sparkles: feat: implement pagination for events route

--- backend/src/routers/event/eventsRoute.ts
+++ backend/src/routers/event/eventsRoute.ts
@@ -9,6 +9,9 @@ router.get(
   '/',
   expressAsyncHandler(async (req: Request, res: Response) => {
     // handle expected query parameters
+    const pageSize = 5;
+    const pageNumber = Number(req.query.pageNumber) || 1;
+
     const name = req.query.name || '';
     const category = req.query.category || '';
     const venue = req.query.venue || '';
@@ -19,15 +22,26 @@ router.get(
     const venueFilter = venue ? { venue } : '';
     const isFeaturedFilter = isFeaturedEvent ? { isFeaturedEvent } : '';
 
-    const events = await Event.find({
+    const count = await Event.count({
       ...nameFilter,
       ...categoryFilter,
       ...venueFilter,
       ...isFeaturedFilter
     });
 
+    const events = await Event.find({
+      ...nameFilter,
+      ...categoryFilter,
+      ...venueFilter,
+      ...isFeaturedFilter
+    })
+      .skip(pageSize * (pageNumber - 1))
+      .limit(pageSize);
+
     if (events.length !== 0) {
-      res.status(200).send(events);
+      res
+        .status(200)
+        .send({ events, pageNumber, pages: Math.ceil(count / pageSize) });
       logger.info(
         `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Sent events.`
       );
