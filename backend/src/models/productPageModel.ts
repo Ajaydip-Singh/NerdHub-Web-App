97dce1d2207e972f7e34e3bf7458569b9aa51aa1
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-05 17:57:49 +0300

    :art: chore: add checkout table styles for product page

--- backend/src/models/productPageModel.ts
+++ backend/src/models/productPageModel.ts
@@ -2,7 +2,16 @@ import mongoose from 'mongoose';
 
 const productPageSchema = new mongoose.Schema(
   {
-    backgroundImage: { type: String, required: true }
+    backgroundImage: { type: String, required: true },
+    productImageBorderColor: { type: String, required: true },
+    tableBorderColor: { type: String, required: true },
+    tableEvenRowBackgroundColor: { type: String, required: true },
+    tableEvenRowTextColor: { type: String, required: true },
+    tableOddRowBackgroundColor: { type: String, required: true },
+    tableOddRowTextColor: { type: String, required: true },
+    checkoutButtonTextColor: { type: String, required: true },
+    checkoutButtonBackgroundColor: { type: String, required: true },
+    checkoutButtonBorderColor: { type: String, required: true }
   },
   {
     timestamps: true
--- backend/src/routers/pages/productPage/editProductPageContent.ts
+++ backend/src/routers/pages/productPage/editProductPageContent.ts
@@ -12,6 +12,31 @@ router.put(
 
     productPageContent.backgroundImage =
       req.body.backgroundImage || productPageContent.backgroundImage;
+    productPageContent.productImageBorderColor =
+      req.body.productImageBorderColor ||
+      productPageContent.productImageBorderColor;
+    productPageContent.tableBorderColor =
+      req.body.tableBorderColor || productPageContent.tableBorderColor;
+    productPageContent.tableEvenRowBackgroundColor =
+      req.body.tableEvenRowBackgroundColor ||
+      productPageContent.tableEvenRowBackgroundColor;
+    productPageContent.tableEvenRowTextColor =
+      req.body.tableEvenRowTextColor ||
+      productPageContent.tableEvenRowTextColor;
+    productPageContent.tableOddRowBackgroundColor =
+      req.body.tableOddRowBackgroundColor ||
+      productPageContent.tableOddRowBackgroundColor;
+    productPageContent.tableOddRowTextColor =
+      req.body.tableOddRowTextColor || productPageContent.tableOddRowTextColor;
+    productPageContent.checkoutButtonTextColor =
+      req.body.checkoutButtonTextColor ||
+      productPageContent.checkoutButtonTextColor;
+    productPageContent.checkoutButtonBackgroundColor =
+      req.body.checkoutButtonBackgroundColor ||
+      productPageContent.checkoutButtonBackgroundColor;
+    productPageContent.checkoutButtonBorderColor =
+      req.body.checkoutButtonBorderColor ||
+      productPageContent.checkoutButtonBorderColor;
 
     const updatedProductPageContent = await productPageContent.save();
 
