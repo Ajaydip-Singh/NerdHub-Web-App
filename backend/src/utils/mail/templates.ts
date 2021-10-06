dac34b77768e7a659b76adb922a73fdd9bae31f1
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-06 13:46:21 +0300

    :bug: fix: send verification email based on domain

--- backend/src/utils/mail/templates.ts
+++ backend/src/utils/mail/templates.ts
@@ -13,10 +13,10 @@ export const confirmEmailTemplate = (
         button: {
           color: '#22BC66', // Optional action button color
           text: 'Confirm your account',
-          link: `http://localhost:3000/login/${user._id}/${confirmationCode}`
+          link: `${process.env.DOMAIN}/login/${user._id}/${confirmationCode}`
         }
       },
-      outro: `If button does not work, go here directly: http://localhost:3000/login/${user._id}/${confirmationCode} Need help, or have questions? Just reply to this email, we'd love to help.`
+      outro: `If button does not work, go here directly: ${process.env.DOMAIN}/login/${user._id}/${confirmationCode} Need help, or have questions? Just reply to this email, we'd love to help.`
     }
   };
 };
