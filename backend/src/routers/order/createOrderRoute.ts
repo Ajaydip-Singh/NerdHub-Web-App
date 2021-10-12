773bf8ff13f1ffa832010726d4f8757f948df183
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-12 16:09:36 +0300

    :sparkles: feat: send customer email upon order creation

--- backend/src/routers/order/createOrderRoute.ts
+++ backend/src/routers/order/createOrderRoute.ts
@@ -4,6 +4,11 @@ import expressAsyncHandler from 'express-async-handler';
 import Order from '../../models/orderModel';
 import { isAuth } from '../../utils/general';
 import logger from '../../utils/logger';
+import nodemailer from 'nodemailer';
+import { mailGenerator } from '../../utils/mail/mail';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { shopOrderReceiptEmailTemplate } from '../../utils/mail/templates';
+import User from '../../models/userModel';
 
 const router = express.Router();
 
@@ -13,16 +18,50 @@ router.post(
   expressAsyncHandler(async (req: GetUserAuthInfoRequest, res: Response) => {
     if (req.body.orderItems.length === 0) {
       res.status(400).send({ message: 'Cart Is Empty' });
-    } else {
-      const order = new Order({
-        orderItems: req.body.orderItems,
-        shippingAddress: req.body.shippingAddress,
-        paymentResult: req.body.paymentResult,
-        totalPrice: req.body.totalPrice,
-        user: req.user._id
+      return;
+    }
+
+    const existingOrder = await Order.findById(req.body._id);
+    if (existingOrder) {
+      res.status(400).send({ message: 'Order exists already' });
+      return;
+    }
+
+    const order = new Order({
+      _id: req.body._id,
+      orderItems: req.body.orderItems,
+      shippingAddress: req.body.shippingAddress,
+      paymentResult: req.body.paymentResult,
+      totalPrice: req.body.totalPrice,
+      user: req.user._id
+    });
+    const createdOrder = await order.save();
+    res.status(200).send(createdOrder);
+
+    const transport = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    } as SMTPTransport.Options);
+
+    const user = await User.findById(req.user._id);
+
+    try {
+      await transport.sendMail({
+        from: process.env.MAIL_FROM,
+        to: `<${user.email}>`,
+        subject: `Nerdhub:  Order ${req.body._id} received`,
+        html: mailGenerator.generate(
+          shopOrderReceiptEmailTemplate(user, req.body.orderItems)
+        )
       });
-      const createdOrder = await order.save();
-      res.status(200).send(createdOrder);
+    } catch (err) {
+      logger.error(
+        `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Cannot send order placement receipt email`
+      );
     }
 
     logger.info(
