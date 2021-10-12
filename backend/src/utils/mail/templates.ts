ab2b15ade0158a19c6022daf72f36a16d3fba7fa
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-12 16:10:15 +0300

    :lipstick: style: create email templates for shop

--- backend/src/utils/mail/templates.ts
+++ backend/src/utils/mail/templates.ts
@@ -20,3 +20,106 @@ export const confirmEmailTemplate = (
     }
   };
 };
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const shopPaymentReceiptEmailTemplateSuccess = (
+  user: User,
+  order: string
+): any => {
+  return {
+    body: {
+      name: `${user.firstName} ${user.lastName}`,
+      intro: `Your payment for order: ${order} has been received`,
+      action: {
+        instructions: `You can check the status of your order and more in your orders dashboard:`,
+        button: {
+          color: '#3869D4',
+          text: `Go to Orders Dashboard`,
+          link: `${process.env.DOMAIN}/shop`
+        }
+      }
+    },
+    outro: 'Thank you for shopping with us.'
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const shopPaymentReceiptEmailTemplateFail = (
+  user: User,
+  order: string
+): any => {
+  return {
+    body: {
+      name: `${user.firstName} ${user.lastName}`,
+      intro: `Your payment for order: ${order} has failed`,
+      action: {
+        instructions:
+          'For security reasons, we are deleting your order since payment was not succesful. Please make sure that your payment details are correct the next time you attempt to pay for the new order.',
+        button: {
+          color: '#3869D4',
+          text: `Go Shopping`,
+          link: `${process.env.DOMAIN}/shop`
+        }
+      }
+    },
+    outro: 'Apologies from Nerdhub for any inconveniences.'
+  };
+};
+
+export const shopOrderReceiptEmailTemplate = (
+  user: User,
+  orderItems: any
+): any => {
+  const items: {
+    name: string;
+    quantity: number;
+    total: number;
+  }[] = [];
+  orderItems.map(
+    (item: {
+      name: string;
+      quantity: number;
+      price: number;
+      taxPrice: number;
+      totalPrice: number;
+    }) => {
+      items.push({
+        name: item.name,
+        quantity: item.quantity,
+        total: (item.price + item.taxPrice) * item.quantity
+      });
+    }
+  );
+
+  return {
+    body: {
+      name: `${user.firstName} ${user.lastName}`,
+      intro: `Your order has been processed successfully.`,
+      table: {
+        data: items,
+        columns: {
+          // Optionally, customize the column widths
+          customWidth: {
+            name: '50%',
+            quantity: '25%',
+            total: '25%'
+          },
+          // Optionally, change column text alignment
+          customAlignment: {
+            price: 'right'
+          }
+        }
+      },
+      action: {
+        instructions:
+          'We will send you an email upon successful payment of order. In the meanwhile you can check the status of your order and more in your orders dashboard:',
+        button: {
+          color: '#3869D4',
+          text: 'Go to Orders Dashboard',
+          link: 'https://mailgen.js/confirm?s=d9729feb74992cc3482b350163a1a010'
+        }
+      },
+      outro: 'We thank you for your order.'
+    }
+  };
+};
