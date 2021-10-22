915839cbcc22aa49ea9abf52f5be9b423a79feda
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-22 23:11:04 +0300

    :sparkles: feat: add pagination to get orders endpoint

--- backend/src/routers/order/getOrdersRoute.ts
+++ backend/src/routers/order/getOrdersRoute.ts
@@ -11,10 +11,17 @@ router.get(
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req: Request, res: Response) => {
-    const orders = await Order.find({}).populate('user', 'firstName');
+    const pageSize = 5;
+    const pageNumber = Number(req.query.pageNumber) || 1;
+
+    const count = await Order.count({}).populate('user', 'email');
+    const orders = await Order.find({})
+      .populate('user', 'email')
+      .skip(pageSize * (pageNumber - 1))
+      .limit(pageSize);
 
     if (orders.length !== 0) {
-      res.send(orders);
+      res.send({ orders, pageNumber, pages: Math.ceil(count / pageSize) });
       logger.info(
         `${req.ip} : ${req.method} : ${req.originalUrl} : ${res.statusCode} : Orders sent succesfully`
       );
