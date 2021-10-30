07787d4a25b22313a42c821fa565c200010e0049
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-30 10:01:08 +0300

    :lock: chore: add helmet for secure headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import logger from './utils/logger';
 import routes from './routers';
 import initializeDatabase from './database';
 import path from 'path';
+import helmet from 'helmet';
 
 // Configure dotenv to use ev from .env
 dotenv.config();
@@ -14,6 +15,7 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 // Configure mongoose
 const DB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/nerdhub';
